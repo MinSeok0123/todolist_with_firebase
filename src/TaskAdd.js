8741f82bf283eee7781837ea83749277284f0ab7
MinSeok0123 <21.minseok.lee@gmail.com>
2022-06-24 10:16:52 +0900

    version1

---
+++ src/TaskAdd.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TaskAdd.css"
+
+const TaskAdd = ({value, changeHandler, clickHandler})=>{
+    return (
+            <form className="field has-addons">
+                <div className="control is-expanded">
+                <input className="input" value={value} onChange={changeHandler}></input>
+                </div>
+                <div className="control">
+                <button className="button is-primary" onClick={clickHandler}>저장</button>
+                </div>
+            </form>
+    )
+}
+export default TaskAdd;
