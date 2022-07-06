ce13671f0d58df301de2cd41665606dbf883d70e
MinSeok0123 <21.minseok.lee@gmail.com>
2022-07-06 16:57:20 +0900

    백엔드 수정

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import "firebase/compat/firestore"
 
 
 const config = {
-    apiKey: "AIzaSyCaGup-JzVuh5NPPIhP5EgbTWSulqLe2CA",
-    authDomain: "todo-ea7a1.firebaseapp.com",
-    databaseURL: "https://todo-ea7a1-default-rtdb.firebaseio.com",
-    projectId: "todo-ea7a1",
-    storageBucket: "todo-ea7a1.appspot.com",
-    messagingSenderId: "858143640446",
-    appId: "1:858143640446:web:2d9d7b5a22dac7b908012f",
-    measurementId: "G-2YFZB2ZVY7"
+    apiKey: "AIzaSyDUKMDXUKalkvGNz5XcUWMMNfwNoTFVxuk",
+    authDomain: "mytodolist-6f0bb.firebaseapp.com",
+    projectId: "mytodolist-6f0bb",
+    storageBucket: "mytodolist-6f0bb.appspot.com",
+    messagingSenderId: "647235277569",
+    appId: "1:647235277569:web:bf91bd8a1913f857128100",
+    measurementId: "G-E0X0LRHTZT"
 };
 
 firebase.initializeApp(config);
